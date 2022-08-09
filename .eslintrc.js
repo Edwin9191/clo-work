aa4f0019fe468f747c3158507096d4e715419414
edwin <mgk9174@naver.com>
2022-08-09 23:58:20 +0900

    feat: project init

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.js'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', '@types'],
+      },
+      typescript: './tsconfig.json',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   singleQuote: true,
+//   semi: true,
+//   useTabs: false,
+//   tabWidth: 2,
+//   trailingComma: "all",
+//   printWidth: 80,
+//   arrowParens: "avoid",
+// };
+
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'always',
+  orderedImports: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+};
