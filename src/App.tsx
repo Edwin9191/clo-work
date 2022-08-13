9acdebbee3311600d881d3fda8b55d3c202a05a9
edwin <mgk9174@naver.com>
2022-08-13 23:21:10 +0900

    feat: 새로고침 시 옵션체크 상태 저장
    - localstorage에 옵션 선택 저장
    - useFilter 커스텀 훅 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,40 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
-import { ClosetContent } from './api/closet';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 import { useActions } from './hooks/useAction';
-import useFilter, { PageData } from './hooks/useFilter';
-import usePagination from './hooks/usePagination';
-import usePricingOption from './hooks/usePricingOption';
-import { useAppSelector } from './hooks/useTypedSelector';
-
-const test = (
-  list: ClosetContent[] | null,
-  pricingOptions: number[],
-  { page, perPage }: PageData,
-) => {
-  let result;
-
-  if (pricingOptions.length) {
-    result = list?.filter((content: ClosetContent) =>
-      pricingOptions.includes(content.pricingOption),
-    );
-  } else {
-    result = list;
-  }
-
-  return result?.filter(
-    (content: ClosetContent, index: number) => index < page * perPage,
-  );
-};
+import useFilter from './hooks/useFilter';
 
 function App() {
   const { getListThunk } = useActions();
-  const { list } = useAppSelector(state => state.closet);
+  const {
+    handleChange,
+    handleCheck,
+    handleClick,
+    pricingOptions,
+    searchKeyword,
+    filteredList,
+    setOptions,
+  } = useFilter();
 
-  const { handleCheck, handleClick, pricingOptions, pageData } = useFilter();
   useEffect(() => {
     getListThunk();
+    setOptions(JSON.parse(localStorage.getItem('options')));
   }, []);
 
-  const filteredList = useMemo(
-    () => test(list.data, pricingOptions, pageData),
-    [pricingOptions, list, pageData],
-  );
-  console.log(filteredList);
+  useEffect(() => {
+    localStorage.setItem('options', JSON.stringify(pricingOptions));
+  }, [pricingOptions]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div>
-      <span>dfsdfadsfsd</span>
+      <input type="text" ref={inputRef} />
+      <button
+        type="button"
+        onClick={() => handleChange(inputRef.current?.value)}
+      >
+        검색
+      </button>
       <button type="button" onClick={() => handleClick()}>
         다음
       </button>
--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,5 +1,6 @@
-import { Dispatch } from 'redux';
-import { useCallback, useState, SetStateAction } from 'react';
+import { useCallback, useState, useMemo } from 'react';
+import { ClosetContent } from '../api/closet';
+import { useAppSelector } from './useTypedSelector';
 
 export interface PageData {
   page: number;
@@ -7,19 +8,70 @@ export interface PageData {
 }
 
 interface FilterProps {
-  pageData: PageData;
+  searchKeyword: string;
   pricingOptions: number[];
+  filteredList: ClosetContent[];
   handleClick: () => void;
   handleCheck: (checked: boolean, option: number) => void;
+  handleChange: (text: string) => void;
+  setOptions: (options: number[]) => void;
 }
+
+interface FilteredListProps {
+  list: ClosetContent[] | null;
+  pricingOptions: number[];
+  pageData?: PageData;
+  searchKeyword: string;
+}
+
+const getFilteredList = ({
+  list,
+  pricingOptions,
+  pageData,
+  searchKeyword,
+}: FilteredListProps) => {
+  let result;
+
+  if (pricingOptions.length) {
+    result = list?.filter((content: ClosetContent) =>
+      pricingOptions.includes(content.pricingOption),
+    );
+  } else {
+    result = list;
+  }
+
+  if (searchKeyword.length) {
+    result = result.filter(
+      content =>
+        content.creator.includes(searchKeyword) ||
+        content.title.includes(searchKeyword),
+    );
+  }
+
+  if (pageData) {
+    result = result?.filter(
+      (content: ClosetContent, index: number) =>
+        index < pageData.page * pageData.perPage,
+    );
+  }
+
+  return result;
+};
 
 const useFilter = (): FilterProps => {
   const [pricingOptions, setPricingOptions] = useState<number[]>([]);
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
   const [pageData, setPageData] = useState({
     page: 1,
     perPage: 12,
   });
+  const {
+    list: { data },
+  } = useAppSelector(state => state.closet);
+
+  const setOptions = useCallback((options: number[]) => {
+    setPricingOptions([...options]);
+  }, []);
 
   const handleCheck = useCallback(
     (checked: boolean, option: number) => {
@@ -28,17 +80,37 @@ const useFilter = (): FilterProps => {
       } else {
         setPricingOptions(pricingOptions.filter(value => value !== option));
       }
-
       setPageData({ ...pageData, page: 1 });
     },
     [pricingOptions],
   );
 
-  const handleClick = useCallback(() => {
-    setPageData({ ...pageData, page: pageData.page + 1 });
-  }, [pageData]);
+  const filteredList = useMemo(
+    () =>
+      getFilteredList({ list: data, pricingOptions, pageData, searchKeyword }),
+    [pricingOptions, data, pageData, searchKeyword],
+  );
 
-  return { pricingOptions, handleCheck, handleClick, pageData };
+  const handleClick = useCallback(() => {
+    const list = getFilteredList({ list: data, pricingOptions, searchKeyword });
+
+    if (!(Math.ceil(list.length / pageData.perPage) < pageData.page + 1))
+      setPageData({ ...pageData, page: pageData.page + 1 });
+  }, [pageData, data, searchKeyword, pricingOptions]);
+
+  const handleChange = useCallback((text: string) => {
+    setSearchKeyword(text);
+  }, []);
+
+  return {
+    handleChange,
+    pricingOptions,
+    handleCheck,
+    handleClick,
+    searchKeyword,
+    filteredList,
+    setOptions,
+  };
 };
 
 export default useFilter;
